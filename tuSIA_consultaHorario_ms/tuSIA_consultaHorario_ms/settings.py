6b5adc8b29772f233902ee29f14163a296d503ed
Julian ro <davidro2121@gmail.com>
2022-10-01 17:38:35 -0500

    changing allowd host

--- tuSIA_consultaHorario_ms/tuSIA_consultaHorario_ms/settings.py
+++ tuSIA_consultaHorario_ms/tuSIA_consultaHorario_ms/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-r5=7pz!d&-a#)zpy#!so5&i3c)7w+(xiz2mdt+!7+0vlix93&m
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '0.0.0.0',
-    '127.0.0.1'
+    '35.193.226.159'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
